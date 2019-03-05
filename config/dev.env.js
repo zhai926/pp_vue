05536f92d30133c069e957a73378dae8564881cc
zhai926 <17317900836@163.com>
2019-03-05 10:55:44 +0800

    pp前端项目第一次提交

---
+++ config/dev.env.js
@@ -0,0 +1,12 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  // API
+  // BASE_API: '"http://47.75.38.171/backend"',
+  BASE_API: '""',
+  // 跳转
+  BASE_HREF: '"http://127.0.0.1:2018"'
+})
